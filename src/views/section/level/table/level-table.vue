57fabf4bb37594836a5345d21fee77fc86aa273f
thak1411 <thak1411@gmail.com>
2021-07-04 04:09:11 +0900

    Update: Level Page Data Loading

--- src/views/section/level/table/level-table.vue
+++ src/views/section/level/table/level-table.vue
@@ -78,10 +78,8 @@ export default {
                 return res.data.result;
             })
             .then(list => {
-                console.log('list:', list)
                 for (let i = 0; i <= 30; ++i) {
                     this.tableItems.push([i, list[i].todo, list[i].solved, list[i].all, list[i].progress]);
-                    console.log('tableItems[i]', this.tableItems[i]);
                 }
             })
             .catch(err => {
